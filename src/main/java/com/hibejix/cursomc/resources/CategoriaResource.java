80166e8b1fd96f18355feeba5936a376fd15964d
Marcelo Salvador <macrusal@gmail.com>
2018-05-25 05:04:12 -0300

    REST - CategoriaResource

---
+++ src/main/java/com/hibejix/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.hibejix.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author msalvador
+ *
+ */
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST esta funcionando!";
+	}
+
+}
