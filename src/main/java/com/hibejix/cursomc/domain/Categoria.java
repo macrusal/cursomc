a64905d730f631bb0243e6a70003541f39abbcc3
Marcelo Salvador <macrusal@gmail.com>
2018-05-26 02:51:04 -0300

    Dependencias no pom.xml para trabalhar com o Banco de Dados H2 e mapeamento da classe Categoria.java

--- src/main/java/com/hibejix/cursomc/domain/Categoria.java
+++ src/main/java/com/hibejix/cursomc/domain/Categoria.java
@@ -5,14 +5,22 @@ package com.hibejix.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author msalvador
  *
  */
+@Entity
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
