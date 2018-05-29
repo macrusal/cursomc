520da300dd30f524008ac1d8691332c215f548f2
Marcelo Salvador <macrusal@gmail.com>
2018-05-29 06:09:51 -0300

    Inserindo Cliente e suas associacoes

--- src/main/java/com/hibejix/cursomc/services/ClienteService.java
+++ src/main/java/com/hibejix/cursomc/services/ClienteService.java
@@ -3,6 +3,7 @@
  */
 package com.hibejix.cursomc.services;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,9 +14,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.hibejix.cursomc.domain.Cidade;
 import com.hibejix.cursomc.domain.Cliente;
+import com.hibejix.cursomc.domain.Endereco;
+import com.hibejix.cursomc.domain.enums.TipoCliente;
 import com.hibejix.cursomc.dto.ClienteDTO;
+import com.hibejix.cursomc.dto.ClienteNewDTO;
+import com.hibejix.cursomc.repositories.CidadeRepository;
 import com.hibejix.cursomc.repositories.ClienteRepository;
+import com.hibejix.cursomc.repositories.EnderecoRepository;
 import com.hibejix.cursomc.services.exceptions.ClienteNotFoundException;
 import com.hibejix.cursomc.services.exceptions.DataIntegrityException;
 
@@ -27,13 +34,31 @@ import com.hibejix.cursomc.services.exceptions.DataIntegrityException;
 public class ClienteService {
 
 	@Autowired
-	ClienteRepository repository;
+	ClienteRepository clienteRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
+	
+	
+	@Autowired
+	EnderecoRepository enderecoRepository;
 	
 	public Cliente find(final Integer id) {
 		
-		Optional<Cliente> cliente = repository.findById(id);
+		Optional<Cliente> cliente = clienteRepository.findById(id);
 		return cliente.orElseThrow(() -> new ClienteNotFoundException(
 				"Cliente não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	/**
+	 * @param cliente
+	 * @return
+	 */
+	public Cliente insert(Cliente cliente) {
+		cliente.setId(null);
+		cliente = clienteRepository.save(cliente);
+		enderecoRepository.saveAll(cliente.getEnderecos());
+		return cliente;
 	}
 	
 	/**
@@ -43,7 +68,7 @@ public class ClienteService {
 	public Cliente update(Cliente cliente) {
 		Cliente clienteNovo = find(cliente.getId());
 		updateData(clienteNovo, cliente);
-		return repository.save(clienteNovo);
+		return clienteRepository.save(clienteNovo);
 	}
 
 	/**
@@ -53,7 +78,7 @@ public class ClienteService {
 	public void delete(Integer id) {
 		find(id);
 		try {
-			repository.deleteById(id);
+			clienteRepository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir um Cliente que possui associações!");
 		}
@@ -64,7 +89,7 @@ public class ClienteService {
 	 * @return
 	 */
 	public List<Cliente> findAll() {
-		return repository.findAll();
+		return clienteRepository.findAll();
 	}
 	
 	/**
@@ -74,7 +99,7 @@ public class ClienteService {
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		@SuppressWarnings("deprecation")
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		return repository.findAll(pageRequest);
+		return clienteRepository.findAll(pageRequest);
 	}
 	
 	/**
@@ -83,6 +108,26 @@ public class ClienteService {
 	 */
 	public Cliente fromDTO(ClienteDTO dto) {
 		return new Cliente(dto.getId(), dto.getNome(),dto.getEmail(), null, null);
+	}
+	
+	/**
+	 * @param 
+	 * @return
+	 */
+	public Cliente fromDTO(ClienteNewDTO dto) {
+		Cliente cliente = new Cliente(null, dto.getNome(),dto.getEmail(), dto.getCpfOuCnpj(), TipoCliente.toEnum(dto.getTipo()));
+		Optional<Cidade> cidade = cidadeRepository.findById(dto.getCidadeId());
+		Endereco endereco = new Endereco(null, dto.getLogradouro(), dto.getNumero(), dto.getComplemento(), dto.getBairro(), dto.getCep(), cliente, cidade.orElse(null));
+		cliente.getEnderecos().addAll(Arrays.asList(endereco));
+		cliente.getTelefones().addAll(Arrays.asList(dto.getTelefone1()));
+		if(dto.getTelefone2() != null) {
+			cliente.getTelefones().addAll(Arrays.asList(dto.getTelefone2()));
+		}
+		if(dto.getTelefone3() != null) {
+			cliente.getTelefones().addAll(Arrays.asList(dto.getTelefone3()));
+		}
+		
+		return cliente; 
 	}
 	
 	private void updateData(Cliente clienteNovo, Cliente cliente) {
