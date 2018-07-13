23e1ae8cdb0d2bb8baebf95cccaeb8310419d31b
Marcelo Salvador <macrusal@gmail.com>
2018-07-13 03:51:29 -0300

    Configuração inicial Spring Security

---
+++ src/main/java/com/hibejix/cursomc/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.hibejix.cursomc.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author msalvador
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment environment;
+	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**",
+			"/hibejix-comercio/produtos/**",
+			"/hibejix-comercio/categorias/**"
+	};
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		
+		if (Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+			httpSecurity.headers().frameOptions().disable();
+		}
+		
+		httpSecurity.cors().and().csrf().disable();
+		httpSecurity.authorizeRequests()
+		.antMatchers(PUBLIC_MATCHERS).permitAll()
+		.anyRequest().authenticated();
+		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+}
